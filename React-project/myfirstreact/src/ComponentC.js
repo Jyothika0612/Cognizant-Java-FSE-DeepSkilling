20a3d06fe56140ecd0f2f7ea19ade13c8d5c27bc
Jyothika0612 <2300030943cseh@gmail.com>
2026-06-26 00:12:54 +0530

    Replace embedded React project with tracked files

---
+++ React-project/myfirstreact/src/ComponentC.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function ComponentC() {
+
+ const user =
+ useContext(UserContext);
+
+ return (
+  <h1>
+   Welcome {user}
+  </h1>
+ );
+
+}
+
+export default ComponentC;
