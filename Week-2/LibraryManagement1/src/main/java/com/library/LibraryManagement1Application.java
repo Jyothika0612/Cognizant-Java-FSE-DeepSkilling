23280e6256566f98dca5d24c0c4432f5cebf571f
Jyothika0612 <2300030943cseh@gmail.com>
2026-07-06 13:38:43 +0530

    Add Week-2 coursework folders

---
+++ Week-2/LibraryManagement1/src/main/java/com/library/LibraryManagement1Application.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagement1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagement1Application.class, args);
+    }
+}
